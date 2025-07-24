56469ebc80816657ab869b195c5a2460e24775a7
tommylbro <47120557+tommylbro@users.noreply.github.com>
2025-07-24 17:17:38 -0500

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        myth: ['"Cinzel"', 'serif'],
+        body: ['"Playfair Display"', 'serif']
+      },
+      colors: {
+        parchment: "#f9f4e7",
+        bronze: "#b4884d",
+        deep: "#2e1f0e",
+        twilight: "#3a2d17"
+      },
+    },
+  },
+  plugins: [],
+}
